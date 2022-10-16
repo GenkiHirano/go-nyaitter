8359dcbf7b4f4abd9afc3c12b842e026fa0aaac4
GenkiHirano <genki86web@gmail.com>
2022-10-16 16:38:17 +0900

    refactor: 不要な環境変数を削除

--- twitter/config.go
+++ twitter/config.go
@@ -12,7 +12,7 @@ func getTwitterAPI() *anaconda.TwitterApi {
 	loadEnv()
 	anaconda.SetConsumerKey(os.Getenv("CONSUMER_KEY"))
 	anaconda.SetConsumerSecret(os.Getenv("CONSUMER_KEY_SECRET"))
-	api := anaconda.NewTwitterApi(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
+	api := anaconda.NewTwitterApi("","")
 	return api
 }
 
